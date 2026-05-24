bf7b3b6b283a4c3a0e5dcd74fac1dec3b8ef5c6f
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2026-05-24 15:21:19 +0600

    feat: add landing page layout with GlassContainer and HomeMainPage component

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
         <div className="w-full max-w-[750px]">
           {/* Heading Section */}
           <div className="space-y-1 mb-6 md:mb-2 text-center">
-            <h3 className="text-[1rem] md:text-[1.2rem] font-medium text-[#09A6A4]">
+            <h3 className="text-[1rem] md:text-[1.7rem] font-medium text-[#09A6A4]">
               Good Morning Fahim!
             </h3>
-            <h1 className="text-[1.2rem] md:text-[1.5rem] font-bold text-slate-900 dark:text-white tracking-tight">
+            <h1 className="text-[1.2rem] md:text-[1.7rem] font-bold text-slate-900 dark:text-white tracking-tight">
               Swap Currencies Directly With People Worldwide.
             </h1>
-            <p className="text-[0.75rem] md:text-[0.85rem] text-slate-500 dark:text-slate-400 font-normal max-w-[500px] mx-auto">
+            <p className="text-[0.75rem] md:text-[1.0rem] text-slate-500 dark:text-slate-400 font-normal max-w-[500px] mx-auto">
               Faster, cheaper, and more transparent than traditional money
               transfer services.
             </p>
